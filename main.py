a78de679e29802cfc48a1831da7452fb146c1b17
Xiaoqiang Zhou <xiaoqiang.chou@gmail.com>
2013-02-21 20:39:47 +0800

    add xcode project

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from PyObjCTools import AppHelper
 from Foundation import *
+from AppKit import *
 
 
 class MainDelegate(NSObject):
-    fileName = objc.IBOutlet()
-    fileContent = objc.IBOutlet()
+
+    compareWindowController = objc.IBOutlet()
 
     @objc.IBAction
     def openFile_(self, button):
-    	fileToOpen = self.fileName.stringValue()
-    	self.fileContent.setStringValue_(fileToOpen)
+        self.compareWindowController.showWindow_()
+
+
+class CompareWindowController(NSWindowController):
+
+    def init(self):
+        self = super(NSWindowController, self).init()
+        NSBundle.loadNibNamed_owner_("CompareWindow", self)
+        return self
 
 
 if __name__ == "__main__":
